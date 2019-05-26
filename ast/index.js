ca34f755305ee06deea92feed9536837443f7755
Jonathan Peralez <jperalez@gmail.com>
2019-05-26 12:10:44 -0700

    Add babel packages to package.json for AST. Add Mike's compiled JS file.

--- ast/index.js
+++ ast/index.js
@@ -1,2 +1,18 @@
 // Start AST here
+const babelParser = require('@babel/parser'),
+  traverse = require('@babel/traverse'),
+  babelTypes = require('@babel/types'),
+  fs = require('fs'),
+  path = require('path');
+
+const bundlePath = path.join(__dirname, '..', 'samples', 'crud_app', 'compiled', 'compiled.js');
+///Users/jonathanperalez/WebstormProjects/apollo-aperture/samples/crud_app/compiled/compiled.js
+const file = fs.readFileSync(bundlePath, 'utf8');
+
+const ast = babelParser.parse(file, {
+  sourceType: 'module',
+  plugins: [ 'jsx' ]
+});
+
+const myAst = ast;
 // Add comment
