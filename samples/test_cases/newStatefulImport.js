bdcb13f7d1d3954e74a31139d5db50b4f5b79e84
jonathanperalez <jperalez@gmail.com>
2020-05-28 10:45:29 -0700

    Add additional tests

---
+++ samples/test_cases/newStatefulImport.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ImportedComponent = () => {
+  return (
+    <div>
+      Here is the exported component
+    </div>
+  );
+};
+
+export default ImportedComponent;
