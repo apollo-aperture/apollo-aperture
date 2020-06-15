a19771b7b639cc2b7005e09af9961ca469984d40
jonathanperalez <jperalez@gmail.com>
2020-06-15 10:13:12 -0700

    Update complete-traversal-test files to account for more use cases

--- samples/complete-traversal-test/App.js
+++ samples/complete-traversal-test/App.js
@@ -1,38 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ImportedComponent from './Components/Component';
-import DisplayText from './Components/DisplayText';
+import ImportedComponent from './components/ImportedComponent';
+import OuterComponent from './components/OuterComponent';
+import DisplayText from './components/DisplayText';
 
-const arrToDisplay = [
-  {
-    idx: 0,
-    text: 'Foo',
-  },
-  {
-    idx: 1,
-    text: 'Bar',
-  },
-  {
-    idx: 2,
-    text: 'Woo',
-  },
-];
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//   render() {
-//     return(
-//       <div>
-//         <div>
-//           <ImportedComponent />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+// const arrToDisplay = [
+//   {
+//     idx: 0,
+//     text: 'Foo',
+//   },
+//   {
+//     idx: 1,
+//     text: 'Bar',
+//   },
+//   {
+//     idx: 2,
+//     text: 'Woo',
+//   },
+// ];
 
 class App extends Component {
   constructor(props) {
@@ -41,15 +27,32 @@ class App extends Component {
   render() {
     return(
       <div>
-        <ImportedComponent />
-        <p>Here is a text node that should be ignored</p>
-        {arrToDisplay.map(el => (
-          <DisplayText idx={el.idx} text={el.text} />
-        ))}
+        <div>
+          <OuterComponent>
+            <ImportedComponent />
+          </OuterComponent>
+        </div>
       </div>
     );
   }
 }
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return(
+//       <div>
+//         <ImportedComponent />
+//         <p>Here is a text node that should be ignored</p>
+//         {arrToDisplay.map(el => (
+//           <DisplayText idx={el.idx} text={el.text} />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
 
 // function App() {
 //   return (
---
+++ samples/complete-traversal-test/components/OuterComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const OuterComponent = ({ children }) => <div>{children}</div>;
+
+export default OuterComponent;
