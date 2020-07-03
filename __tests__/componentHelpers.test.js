a09229239db89a6718be6ebd351e274e557414c1
jonathanperalez <jperalez@gmail.com>
2020-07-03 10:18:19 -0700

    componentHelpers.test.js: Add tests for componentHelpers.js

---
+++ __tests__/componentHelpers.test.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const path = require('path');
+const parser = require('@babel/parser');
+
+const helpers = require('../traverse/componentHelpers');
+
+function getAST(filename) {
+  const filePath = path.join(
+    __dirname,
+    'apollo-react-files',
+    'fragments',
+    filename
+  );
+  const file = fs.readFileSync(filePath, 'utf8');
+  return parser.parse(file, {
+    sourceType: 'module',
+    plugins: ['jsx'],
+  });
+}
+
+it('finds React components inside of a JSX Expression', () => {
+  const filePath = path.join(
+    __dirname,
+    'apollo-react-files',
+    'fragments',
+    'reactBody.js'
+  );
+  const file = fs.readFileSync(filePath, 'utf8');
+  const ast = parser.parse(file, {
+    sourceType: 'module',
+    plugins: ['jsx'],
+  });
+  const node = ast.program.body[0].expression;
+  expect(helpers.generateComponentTreeFromJSXElement(node)).toEqual({
+    reactComponent: 'A',
+    children: [
+      {
+        reactComponent: 'B',
+        children: [],
+      },
+      {
+        reactComponent: 'C',
+        children: [
+          {
+            reactComponent: 'D',
+            children: [],
+          },
+        ],
+      },
+    ],
+  });
+});
+
+it('finds React and Apollo Components', () => {
+  const ast = getAST('reactAndApollo.js');
+  const node = ast.program.body[0].expression;
+  const result = helpers.generateReactApolloHierarchy(node);
+  expect(result.hierarchy).toEqual({
+    reactComponent: 'React',
+    children: [
+      {
+        reactComponent: 'ApolloProvider',
+        children: [],
+      },
+    ],
+  });
+  expect(result.node).toHaveProperty('type');
+  expect(result.node.type).toEqual('JSXElement');
+});
