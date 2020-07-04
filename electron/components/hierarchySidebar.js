dbe1869813e02836fc8ac904be9dc276581e8f89
jonathanperalez <jperalez@gmail.com>
2020-07-04 10:04:57 -0700

    Update Electron App's outdated NPM packages and update hierarchySidebar.js to callback style

--- electron/components/hierarchySidebar.js
+++ electron/components/hierarchySidebar.js
@@ -2,14 +2,21 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const openDialog = () => {
-  window.electron.dialog.showOpenDialog({properties: ['openDirectory']});
+  window.electron.dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [
+      { name: 'JavaScript Files', extensions: ['js']}
+    ]
+  }).then(result => {
+    console.log('result: ', result);
+  });
 }
 
 const HierarchySidebar = () => (
   <>
     <div className="flex-inner">
       Open directory to generate component hierarchy
-      <button type="button" onClick={openDialog}>Select folder</button>
+      <button type="button" onClick={openDialog}>Select index.js</button>
     </div>
     <div className="flex-inner">
       <hr />
