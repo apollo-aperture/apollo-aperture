0b75f36b4aefd023f1a5dd52ab400b736f8b113c
jonathanperalez <jperalez@gmail.com>
2020-07-14 10:22:49 -0700

    Fix bug where App was imported twice
    This created issues with Electron's renderer

--- samples/complete-traversal-test/index.js
+++ samples/complete-traversal-test/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { ApolloProvider } from '@apollo/react-hooks';
 import App from './App';
-import App from './App';
 import client from './client';
 import * as serviceWorker from './serviceWorker';
 
